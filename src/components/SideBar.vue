fceca0ba9996fb30f92c372d4353ceb4b74033b9
GunnerSaurusBJJ <ddaa.99ars@yandex.ru>
2020-09-08 22:17:38 +0300

    Added dynamic date and navbar + sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isSideBarOpened}">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      active-class="active"
+      tag="li"
+      exact
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/" },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+  props: {
+    isSideBarOpened: {
+      type: Boolean,
+      required: true
+    }
+  },
+};
+</script>
+
+<style>
+</style>
