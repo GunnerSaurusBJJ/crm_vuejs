69fcbfbf3c33a93d130372ed5191e16681a4ee28
GunnerSaurusBJJ <ddaa.99ars@yandex.ru>
2020-09-09 21:42:13 +0300

    add validation vuelidate

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
 import 'materialize-css/dist/js/materialize.min'
 Vue.config.productionTip = false
+
 Vue.filter('date', dateFilter)
+Vue.use(Vuelidate)
 new Vue({
   router,
   store,
